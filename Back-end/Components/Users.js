63dec8350a83e4b48ccbcb8da7f33c95943c71af
Mahi singh <aka.mahisingh@gmail.com>
2025-01-28 10:32:29 +0530

    1.005

--- Back-end/Components/Users.js
+++ Back-end/Components/Users.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcryptjs from 'bcryptjs';
 import mongoose from 'mongoose';
 
 // Define the user schema
@@ -23,27 +23,23 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true, minlength: 8 },
 });
 
-// Hash password before saving the user
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  try {
-    const hashedPassword = await bcrypt.hash(this.password, 10);
-    this.password = hashedPassword;
-    next();
-  } catch (error) {
-    next(error);
+// Hash the user's password before saving it to the database
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password') || this.isNew) {
+    this.password = await bcryptjs.hash(this.password, 10);
   }
+  next();
 });
 
-// Method to check if the entered password is valid
-userSchema.methods.isValidPassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
+// Method to compare password for login
+userSchema.methods.comparePassword = async function(password) {
+  return await bcryptjs.compare(password, this.password);
 };
 
-// Generate a JWT token
-userSchema.methods.generateToken = function () {
-  const payload = { id: this._id, email: this.emailAddress, name: this.fullName };
-  const secretKey = process.env.JWT_SECRET || 'default_secret';
+// Method to generate JWT token
+userSchema.methods.generateAuthToken = function() {
+  const payload = { id: this._id, email: this.emailAddress };
+  const secretKey = process.env.JWT_SECRET_KEY || 'yourSecretKey';
   const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
   return token;
 };
